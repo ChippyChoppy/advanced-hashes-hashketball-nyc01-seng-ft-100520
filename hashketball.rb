1815e74a1f36c34c5dac97a41510ffc907504cde
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-10-02 15:39:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -131,11 +131,11 @@ end
 # Write code here
 
 def num_points_scored(player_name)
-  binding.pry
+  #binding.pry
   game_hash.each do |location, team_data|
-    binding.pry
+   # binding.pry
     team_data.each do |attribute, data|
-      binding.pry
+    #  binding.pry
       data.each do |data_item|
         binding.pry
       end
