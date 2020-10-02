ce393aeb2ddbee26e36ad41204e06ac0fc185421
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-10-02 16:25:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -190,5 +190,6 @@ def player_numbers(team_name)
   player_jersey = [ ]
   game_hash.each do |location, team_stats|
     if team_stats[:team_name] == team_name
-      
+      team_stats[:players].each do
+        player_jersey<< players[:number]
       
